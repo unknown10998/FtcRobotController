2e25febef878dc68ecc0c65e37bd38c84370dcfe
unknown10998 <290444990+unknown10998@users.noreply.github.com>
2026-07-05 13:20:01 -0500

    Touch Sensors
    added ts

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TouchSensorPractice.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
+
+@TeleOp
+public class TouchSensorPractice extends OpMode {
+    private final TestBench bench = new TestBench();
+
+    @Override
+    public void init() {
+        bench.init(hardwareMap);
+    }
+
+    @Override
+    public void loop(){
+        boolean tsPressed = bench.isTouchSensorPressed();
+        boolean tsReleased = bench.isTouchSensorReleased();
+        telemetry.addData("Touch Sensor Pressed State", tsPressed);
+        telemetry.addData("Touch Sensor Released State", tsReleased);
+        if (tsPressed && !tsReleased) telemetry.addData("pressed?", "pressed");
+        else telemetry.addData("pressed?", "not pressed");
+        telemetry.update();
+    }
+}
